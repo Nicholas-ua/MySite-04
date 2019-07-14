89b9eda84a44f0fc393e7290865f51eef1f72ba4
Nicholas-ua <m.savych@gmail.com>
2019-07-14 13:20:17 +0300

    Add functions

--- js/robot.js
+++ js/robot.js
@@ -1,4 +1,4 @@
-function allowDrop(ev) {
+/*function allowDrop(ev) {
     ev.preventDefault();
   }
   
@@ -10,4 +10,99 @@ function allowDrop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-  }
+  }*/
+
+  function hideDivs0()
+{
+    document.getElementById('drag1').style.display = 'none';
+    document.getElementById('step00').style.display = 'block';
+}
+
+function hideDivs1()
+{
+    if (document.getElementById('step00').style.display === 'block')
+    {
+        document.getElementById('drag1').style.display = 'none';
+        document.getElementById('step00').style.display = 'none';
+        document.getElementById('drag2').style.display = 'none';
+        document.getElementById('step01').style.display = 'block';
+        /*this.style.display = 'none';*/
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs2()
+{
+    if (document.getElementById('step01').style.display === 'block')
+    {
+        document.getElementById('drag3').style.display = 'none';
+        document.getElementById('step01').style.display = 'none';
+        document.getElementById('step02').style.display = 'block';
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs3()
+{
+    if (document.getElementById('step02').style.display === 'block')
+    {
+        document.getElementById('step02').style.display = 'none';
+        document.getElementById('drag4').style.display = 'none';
+        document.getElementById('step03').style.display = 'block';
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs4()
+{
+    if (document.getElementById('step03').style.display === 'block')
+    {
+        document.getElementById('step03').style.display = 'none';
+        document.getElementById('drag5').style.display = 'none';
+        document.getElementById('step04').style.display = 'block';
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs5()
+{
+    if (document.getElementById('step04').style.display === 'block')
+    {
+        document.getElementById('step04').style.display = 'none';
+        document.getElementById('drag6').style.display = 'none';
+        document.getElementById('step05').style.display = 'block';
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs6()
+{
+    if (document.getElementById('step05').style.display === 'block')
+    {
+        document.getElementById('step05').style.display = 'none';
+        document.getElementById('drag7').style.display = 'none';
+        document.getElementById('step06').style.display = 'block';
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs7()
+{
+    if (document.getElementById('step06').style.display === 'block')
+    {
+        document.getElementById('step06').style.display = 'none';
+        document.getElementById('drag8').style.display = 'none';
+        document.getElementById('step07').style.display = 'block';
+        document.getElementById('message').innerHTML = 'Congratulation! You have assembled the Robot!';
+    }
+    else
+        alert('Try another part!');
+}
+
+
